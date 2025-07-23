48f849a6ab2a6b097122a8b88fbbe509b57193e7
arya5243 <aj375067@gmail.com>
2025-07-23 22:02:31 +0530

    Quick update

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -57,12 +57,12 @@ const Contact = () => {
       <div className="relative min-h-screen flex items-center justify-center flex-col  mt-5 ">
           <img src="/3d_Protfolio/assets/terminal.png" alt="" className="absolute inset-0 min-h-screen"/>
           <div className="contact-container">
-            <h3 className="head-text">Let's Talk</h3>
+            <h3 className="head-text mt-10">Let's Talk</h3>
             <p className="text-lg text-white-600 mt-3">
               Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
               life, I’m here to help.
             </p>
-            <form ref={formRef} onSubmit={handleSubmit} className="mt-[10%] flex flex-col space-y-7">
+            <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
                 <label htmlFor="" className="space-y-3 ">
                   <span className="field-label">Full Name</span>
                   <input type="text" name="name" value={form.name} onChange={handleChange} required className="field-input" placeholder="John xyz"/>
